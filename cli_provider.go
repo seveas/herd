b76e6f579131d7fe188e9380be33322c615af18b
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2019-04-12 16:02:28 +0200

    Add a provider that accepts any argument that resolves

---
+++ cli_provider.go
@@ -0,0 +1,21 @@
+package katyusha
+
+import (
+	"golang.org/x/crypto/ssh"
+	"net"
+)
+
+type CliProvider struct {
+}
+
+func NewCliProvider() *CliProvider {
+	return &CliProvider{}
+}
+
+func (p *CliProvider) GetHosts(name string, attributes HostAttributes) Hosts {
+	if _, err := net.LookupHost(name); err != nil {
+		return Hosts{}
+	}
+	return Hosts{NewHost(name, []ssh.PublicKey{}, HostAttributes{})}
+
+}
