6fbc5539d30b0eded72f39236e70b4f543ff46f5
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2019-12-11 20:30:32 +0100

    Parse openssh config, for now only user/port are actually used

---
+++ sshconfig.go
@@ -0,0 +1,78 @@
+package katyusha
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+type sshConfig []sshConfigBlock
+
+type sshConfigBlock struct {
+	glob   string
+	config map[string]string
+}
+
+// Parse an openssh config file
+func parseSshConfig(file string) (*sshConfig, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return &sshConfig{}, err
+	}
+	// Anything before the first Host section is global
+	ret := sshConfig{}
+	conf := make(map[string]string)
+	hosts := []string{"*"}
+
+	for _, line := range strings.Split(string(data), "\n") {
+		line = strings.TrimSpace(line)
+		// Ignore comments
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+		parts := strings.SplitN(line, " ", 2)
+		if len(parts) != 2 {
+			logrus.Errorf("Ignoring invalid ssh config line: %s", line)
+			continue
+		}
+		key := strings.ToLower(strings.TrimSpace(parts[0]))
+		val := strings.TrimSpace(parts[1])
+		// New host section
+		if key == "host" {
+			for _, h := range hosts {
+				ret = append(ret, sshConfigBlock{glob: h, config: conf})
+			}
+			conf = make(map[string]string)
+			hosts = strings.Split(val, " ")
+			for i, h := range hosts {
+				hosts[i] = strings.TrimSpace(h)
+			}
+			continue
+		}
+		// Can't overwrite things, first value rules
+		if _, ok := conf[key]; !ok {
+			conf[key] = val
+		}
+	}
+	for _, h := range hosts {
+		ret = append(ret, sshConfigBlock{glob: h, config: conf})
+	}
+	return &ret, nil
+}
+
+// Find all variables relevant for a host, first match wins
+func (s *sshConfig) configForHost(name string) map[string]string {
+	ret := make(map[string]string)
+	for _, b := range *s {
+		if ok, err := filepath.Match(b.glob, name); ok && err == nil {
+			for k, v := range b.config {
+				if _, ok := ret[k]; !ok {
+					ret[k] = v
+				}
+			}
+		}
+	}
+	return ret
+}
