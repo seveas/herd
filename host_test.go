fb2db103798c332fa0f19e9b2511e63405182bb5
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2019-12-11 21:03:27 +0100

    Add host deserialization tests

---
+++ host_test.go
@@ -0,0 +1,25 @@
+package katyusha
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestHostDeserialization(t *testing.T) {
+	data := map[string]interface{}{
+		"Name": "test-host",
+	}
+	bdata, _ := json.Marshal(data)
+	var host Host
+	err := json.Unmarshal(bdata, &host)
+	if err != nil {
+		t.Errorf("Unable to deserialize host data: %s", err)
+	}
+	host.init()
+	if host.Attributes == nil {
+		t.Errorf("Deserialized host does not get attributes")
+	}
+	if host.Port != 22 {
+		t.Errorf("Deserialized host does not get default port")
+	}
+}
