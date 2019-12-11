314576226bc8e3235239b97b4fda70806c1dc56e
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2019-12-11 15:19:18 +0100

    Split value matching into its own file and test it
    This also resulted in fixing a few bugs in the new slice matching logic.

---
+++ matcher.go
@@ -0,0 +1,87 @@
+package katyusha
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+)
+
+type MatchAttribute struct {
+	Name        string
+	FuzzyTyping bool
+	Negate      bool
+	Regex       bool
+	Value       interface{}
+}
+
+func (m MatchAttribute) String() string {
+	c1, c2 := '=', '='
+	if m.FuzzyTyping {
+		c1, c2 = '≈', '≈'
+	}
+	if m.Negate {
+		c1 = '!'
+	}
+	if m.Regex {
+		c2 = '~'
+	}
+	return fmt.Sprintf("%v %c%c %v", m.Name, c1, c2, m.Value)
+}
+
+func (m MatchAttribute) Match(value interface{}) (matches bool) {
+	defer func() {
+		if m.Negate {
+			matches = !matches
+		}
+	}()
+	if svalue := reflect.ValueOf(value); svalue.Kind() == reflect.Slice {
+		// Here we ignore Negate to make sure we filter for any/none matching
+		mx := MatchAttribute{Name: m.Name, Value: m.Value, FuzzyTyping: m.FuzzyTyping, Regex: m.Regex}
+		for i := 0; i < svalue.Len(); i++ {
+			if mx.Match(svalue.Index(i).Interface()) {
+				return true
+			}
+		}
+		return false
+	}
+	if m.Value == value {
+		return true
+	}
+	if m.Regex {
+		svalue, ok := value.(string)
+		return ok && m.Value.(*regexp.Regexp).MatchString(svalue)
+	}
+	if m.FuzzyTyping {
+		if bvalue, ok := value.(bool); ok && (m.Value == "true" || m.Value == "false") {
+			return bvalue == (m.Value == "true")
+		}
+		if m.Value == "nil" {
+			return value == nil
+		}
+		myival, err := strconv.ParseInt(m.Value.(string), 0, 64)
+		if err != nil {
+			return false
+		}
+		v := reflect.ValueOf(value)
+		switch v.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return v.Int() == myival
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			return int64(v.Uint()) == myival
+		}
+	}
+	// Let's be gentle on all the int types in attributes
+	if myival, ok := m.Value.(int64); ok {
+		v := reflect.ValueOf(value)
+		switch v.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return v.Int() == myival
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			return int64(v.Uint()) == myival
+		}
+	}
+	return false
+}
+
+type MatchAttributes []MatchAttribute
