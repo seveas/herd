8cadd450293bc7b18c45bbca3f9440c7bc403d61
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2020-01-24 20:13:56 +0100

    Change history filenames to something not illegal on windows

--- cmd/katyusha/interactive.go
+++ cmd/katyusha/interactive.go
@@ -43,7 +43,7 @@ func runInteractive(cmd *cobra.Command, args []string) error {
 		logrus.Error(err.Error())
 		return err
 	}
-	fn := filepath.Join(currentUser.historyDir, time.Now().Format("2006-01-02T15:04:05.json"))
+	fn := filepath.Join(currentUser.historyDir, time.Now().Format("2006-01-02_150405.json"))
 	engine.Execute()
 
 	// Enter interactive mode
