eb081b1bdfd44dae9e196d2c981591c6db6d4600
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2020-12-30 10:30:26 +0100

    Prefer integers over floats when decoding json-serialized host attributes
    We only support matching on integers anyway.

--- host_test.go
+++ host_test.go
@@ -8,6 +8,11 @@ import (
 func TestHostDeserialization(t *testing.T) {
 	data := map[string]interface{}{
 		"Name": "test-host.katyusha.ci",
+		"Attributes": map[string]interface{}{
+			"color":  "puce",
+			"number": 32,
+			"float":  1.1,
+		},
 	}
 	bdata, _ := json.Marshal(data)
 	var host Host
@@ -23,6 +28,15 @@ func TestHostDeserialization(t *testing.T) {
 	}
 	if host.Port != 22 {
 		t.Errorf("Deserialized host does not get default port")
+	}
+	if color, ok := host.Attributes["color"]; !ok || color != "puce" {
+		t.Errorf("string attribute did not survive the json trip: %v", host.Attributes)
+	}
+	if number, ok := host.Attributes["number"]; !ok || number != int64(32) {
+		t.Errorf("integer attribute did not survive the json trip: %v", host.Attributes)
+	}
+	if flt, ok := host.Attributes["float"]; !ok || flt != 1.1 {
+		t.Errorf("float attribute did not survive the json trip: %v", host.Attributes)
 	}
 }
 
