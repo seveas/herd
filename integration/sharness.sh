ebaf5b161d30cbc1408a1d95c4ee42e7e09c12e1
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2020-12-30 21:21:13 +0100

    Add an actual ssh integration test

--- integration/sharness.sh
+++ integration/sharness.sh
@@ -1,3 +1,7 @@
 SHARNESS_TEST_SRCDIR=sharness
 . "$SHARNESS_TEST_SRCDIR/sharness.sh"
 DATADIR="$SHARNESS_TEST_DIRECTORY/$this_test"
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    eval $(ssh-agent)
+    ssh-add "$SHARNESS_TEST_DIRECTORY"/openssh/user.key
+fi
