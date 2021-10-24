103dd39d424625277d13028f87af378c44f61134
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2021-10-24 11:24:21 +0200

    Handle attributes with : in the name better when sampling
    While we're adding tests, rewrite them to follow go standards.

--- scripting/engine_test.go
+++ scripting/engine_test.go
@@ -2,6 +2,7 @@ package scripting
 
 import (
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/go-test/deep"
@@ -13,100 +14,133 @@ func init() {
 }
 
 func TestParseCommandLine(t *testing.T) {
-	tests := [][]string{
-		{"*"},
-		{"+", "*"},
-		{"*", "foo=bar"},
-		{"*", "foo=bar", "baz=quux"},
-		{"*", "foo=bar", "+", "*", "baz=quux"},
-		{"*", "foo=bar", "-", "*", "baz=quux"},
-		{"*", "foo=bar", "-", "*", "baz=quux", "+", "*", "zoinks=floop"},
-		{"*", "foo"},
-		{"*", "foo!=bar"},
-		{"*", "foo=~bar"},
-		{"*", "foo!~bar"},
-		{"foo=bar"},
-		{"foo=bar", "+", "baz=quux"},
-	}
-	expected := [][]command{
+	tests := []struct {
+		spec []string
+		cmd  []command
+		err  string
+	}{
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{}, sampled: []string{}},
-		},
-		{},
-		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
+			spec: []string{"*"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{}, sampled: []string{}}},
+			err:  "",
 		},
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}, {Name: "baz", Value: "quux", FuzzyTyping: true}}, sampled: []string{}},
+			spec: []string{"+", "*"},
+			cmd:  []command{},
+			err:  "incorrect filter: *",
 		},
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}, sampled: []string{}},
+			spec: []string{"*", "foo=bar"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}}},
+			err:  "",
 		},
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
-			removeHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}},
+			spec: []string{"*", "foo=bar", "baz=quux"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}, {Name: "baz", Value: "quux", FuzzyTyping: true}}, sampled: []string{}}},
+			err:  "",
 		},
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
-			removeHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}},
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "zoinks", Value: "floop", FuzzyTyping: true}}, sampled: []string{}},
-		},
-		{},
-		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true, Negate: true}}, sampled: []string{}},
-		},
-		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: regexp.MustCompile("bar"), Regex: true}}, sampled: []string{}},
+			spec: []string{"*", "foo=bar", "+", "*", "baz=quux"},
+			cmd: []command{
+				addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
+				addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}, sampled: []string{}},
+			},
+			err: "",
 		},
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: regexp.MustCompile("bar"), Regex: true, Negate: true}}, sampled: []string{}},
+			spec: []string{"*", "foo=bar", "-", "*", "baz=quux"},
+			cmd: []command{
+				addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
+				removeHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}},
+			},
+			err: "",
 		},
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
+			spec: []string{"*", "foo=bar", "-", "*", "baz=quux", "+", "*", "zoinks=floop"},
+			cmd: []command{
+				addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
+				removeHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}},
+				addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "zoinks", Value: "floop", FuzzyTyping: true}}, sampled: []string{}},
+			},
+			err: "",
 		},
 		{
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
-			addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}, sampled: []string{}},
+			spec: []string{"*", "foo"},
+			cmd:  []command{},
+			err:  "incorrect filter: foo",
 		},
-	}
-	errors := []string{
-		"",
-		"incorrect filter: *",
-		"",
-		"",
-		"",
-		"",
-		"",
-		"incorrect filter: foo",
-		"",
-		"",
-		"",
-		"",
-		"",
+		{
+			spec: []string{"*", "foo!=bar"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true, Negate: true}}, sampled: []string{}}},
+			err:  "",
+		},
+		{
+			spec: []string{"*", "foo=~bar"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: regexp.MustCompile("bar"), Regex: true}}, sampled: []string{}}},
+			err:  "",
+		},
+		{
+			spec: []string{"*", "foo!~bar"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: regexp.MustCompile("bar"), Regex: true, Negate: true}}, sampled: []string{}}},
+			err:  "",
+		},
+		{
+			spec: []string{"foo=bar"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}}},
+			err:  "",
+		},
+		{
+			spec: []string{"foo=bar", "+", "baz=quux"},
+			cmd: []command{
+				addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "foo", Value: "bar", FuzzyTyping: true}}, sampled: []string{}},
+				addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{{Name: "baz", Value: "quux", FuzzyTyping: true}}, sampled: []string{}},
+			},
+			err: "",
+		},
+		{
+			spec: []string{"foo:1"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{}, sampled: []string{"foo"}, count: 1}},
+			err:  "",
+		},
+		{
+			spec: []string{"foo:bar:1"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{}, sampled: []string{"foo", "bar"}, count: 1}},
+			err:  "",
+		},
+		{
+			spec: []string{":::foo:bar::baz:::quux:3"},
+			cmd:  []command{addHostsCommand{glob: "*", attributes: katyusha.MatchAttributes{}, sampled: []string{"foo", "bar:baz:", "quux"}, count: 3}},
+			err:  "",
+		},
+		{
+			spec: []string{"foo:1", "bar:1"},
+			cmd:  []command{},
+			err:  "only one sampling per hostspec allowed",
+		},
 	}
 
-	for i, test := range tests {
-		e := NewScriptEngine(nil, nil)
-		err := e.ParseCommandLine(test, -1)
-		if (err != nil && err.Error() != errors[i]) || (err == nil && errors[i] != "") {
-			t.Errorf("Unexpected error %v, expected %v", err, errors[i])
-		}
-		if diff := deep.Equal(e.commands, expected[i]); diff != nil {
-			t.Error(diff)
-		}
-		if errors[i] != "" {
-			continue
-		}
-		test = append(test, "id", "seveas")
-		e = NewScriptEngine(nil, nil)
-		err = e.ParseCommandLine(test, len(test)-2)
-		if (err != nil && err.Error() != errors[i]) || (err == nil && errors[i] != "") {
-			t.Errorf("Unexpected error %v, expected %v", err, errors[i])
-		}
-		if diff := deep.Equal(e.commands, append(expected[i], runCommand{command: "id seveas"})); diff != nil {
-			t.Errorf("%v", test)
-			t.Error(diff)
-		}
+	for _, test := range tests {
+		t.Run(strings.Join(test.spec, " "), func(t *testing.T) {
+			e := NewScriptEngine(nil, nil)
+			err := e.ParseCommandLine(test.spec, -1)
+			if (err != nil && err.Error() != test.err) || (err == nil && test.err != "") {
+				t.Errorf("Unexpected error %v, expected %v", err, test.err)
+			}
+			if diff := deep.Equal(test.cmd, e.commands); diff != nil {
+				t.Error(diff)
+			}
+			if test.err != "" {
+				return
+			}
+			test.spec = append(test.spec, "id", "seveas")
+			e = NewScriptEngine(nil, nil)
+			err = e.ParseCommandLine(test.spec, len(test.spec)-2)
+			if (err != nil && err.Error() != test.err) || (err == nil && test.err != "") {
+				t.Errorf("Unexpected error %v, expected %v", err, test.err)
+			}
+			if diff := deep.Equal(e.commands, append(test.cmd, runCommand{command: "id seveas"})); diff != nil {
+				t.Error(diff)
+			}
+		})
 	}
 }
