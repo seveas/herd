378280c0ecb2ae138c0520e6bbdfd6480f4227b8
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2025-08-07 16:45:10 +0200

    Don't show statistics by default

--- integration/t2001_consul.t
+++ integration/t2001_consul.t
@@ -16,20 +16,20 @@ test_description="Consul provider"
 
 test_expect_success "We see hosts using the consul provider" "
     export CONSUL_HTTP_ADDR=http://consul-server-dc1.example.com:8500 &&
-    echo 'datacenter   count   percentage' > expected &&
-    echo 'dc1          6       50%       ' >> expected &&
-    echo 'dc2          6       50%       ' >> expected &&
-	echo 'total        12                ' >> expected &&
+    echo 'datacenter   count' > expected &&
+    echo 'dc1          6    ' >> expected &&
+    echo 'dc2          6    ' >> expected &&
+    echo 'total        12   ' >> expected &&
     herd list --refresh herd_provider=consul --count datacenter --sort datacenter > actual &&
     test_cmp expected actual
 "
 
 test_expect_success "We see services using the consul provider" "
     export CONSUL_HTTP_ADDR=http://consul-server-dc1.example.com:8500 &&
-    echo 'service    count   percentage' > expected &&
-    echo '[consul]   2       16.67%    ' >> expected &&
-    echo '<nil>      10      83.33%    ' >> expected &&
-	echo 'total      12                ' >> expected &&
+    echo 'service    count' > expected &&
+    echo '[consul]   2    ' >> expected &&
+    echo '<nil>      10   ' >> expected &&
+    echo 'total      12   ' >> expected &&
     herd list herd_provider=consul --count service --sort service > actual &&
     test_cmp expected actual
 "
--- ui.go
+++ ui.go
@@ -196,6 +196,7 @@ type HostListOptions struct {
 	Count         []string
 	SortByCount   bool
 	Group         string
+	Stats         bool
 }
 
 type SimpleUI struct {
@@ -620,9 +621,15 @@ func (ui *SimpleUI) PrintHostList(opts HostListOptions) {
 			copy(attrline, opts.Count)
 			copy(attrline[len(opts.Count):], groups)
 			if len(groups) == 0 {
-				attrline = append(attrline, "count", "percentage")
+				attrline = append(attrline, "count")
+				if opts.Stats {
+					attrline = append(attrline, "percentage")
+				}
 			} else {
-				attrline = append(attrline, "total", "percentage", "average", "stddev")
+				attrline = append(attrline, "total")
+				if opts.Stats {
+					attrline = append(attrline, "percentage", "average", "stddev")
+				}
 			}
 			writer.Write(attrline)
 		}
@@ -633,9 +640,12 @@ func (ui *SimpleUI) PrintHostList(opts HostListOptions) {
 				v = append(v, fmt.Sprintf("%v", groupCounts[k][group]))
 				stats.Add(float64(groupCounts[k][group]))
 			}
-			v = append(v, fmt.Sprintf("%v", counts[k]), fmt.Sprintf("%v%%", f2s(float64(counts[k])/float64(total)*100)))
-			if stats.NumDataValues() != 0 {
-				v = append(v, f2s(stats.Mean()), f2s(stats.StandardDeviationPopulation()))
+			v = append(v, fmt.Sprintf("%v", counts[k]))
+			if opts.Stats {
+				v = append(v, fmt.Sprintf("%v%%", f2s(float64(counts[k])/float64(total)*100)))
+				if stats.NumDataValues() != 0 {
+					v = append(v, f2s(stats.Mean()), f2s(stats.StandardDeviationPopulation()))
+				}
 			}
 
 			writer.Write(v)
@@ -645,7 +655,7 @@ func (ui *SimpleUI) PrintHostList(opts HostListOptions) {
 			for range len(opts.Count) - 1 {
 				v = append(v, "")
 			}
-			if opts.Group != "" {
+			if opts.Stats && opts.Group != "" {
 				stats := variance.New()
 				for _, group := range groups {
 					v = append(v, fmt.Sprintf("%v", totals[group]))
@@ -653,7 +663,10 @@ func (ui *SimpleUI) PrintHostList(opts HostListOptions) {
 				}
 				v = append(v, fmt.Sprintf("%v", total), "", f2s(stats.Mean()), f2s(stats.StandardDeviationPopulation()))
 			} else {
-				v = append(v, fmt.Sprintf("%v", total), "")
+				v = append(v, fmt.Sprintf("%v", total))
+				if opts.Stats {
+					v = append(v, "")
+				}
 			}
 			writer.Write(v)
 		}
